ce72f928f29686432b13a9845aad44391c3c23d5
Lordva <lordva.pro@gmail.com>
2022-04-21 16:49:22 +0200

    Exo 2 et 3 parcours graph

--- TD/TD_parcours_graph.py
+++ TD/TD_parcours_graph.py
@@ -1,3 +1,8 @@
+## Exercice 1
+
+from collections import deque
+
+from numpy import empty
 
 
 mat_adj=[[0,1,1,0,0,0,0,0,0,1],
@@ -26,9 +31,51 @@ def determination_numero(etiquettes, nom_neud):
         if etiquettes[i] == nom_neud:
             return i
 
-def recherche_voisins(m,neud_visites,neud):
+def recherche_voisins(m,neud_visites,noeud):
     L=[]
-    for i in range(len(mat_adj)):
-        if mat_adj[neud][i] != 0 and i not in neud_visites:
+    for i in range(len(m)):
+        if m[noeud][i] != 0 and i not in neud_visites:
             L += [i]
     return L
+
+## Exercice 2: Parcours en largeur
+
+def parcours_largeur(mat):
+    file = deque(mat[0])
+    n_visite = [file[0]]
+    while len(file) != 0:
+        noeud_courant = file.popleft()
+        voisins = recherche_voisins(mat,n_visite,noeud_courant)
+        file.extend(voisins)
+        n_visite.extend(voisins)
+    return n_visite
+
+print(parcours_largeur(mat_adj))
+
+def parcour_profondeur(mat):
+    pile = deque(mat[0])
+    n_visite = [pile[0]]
+    while len(pile) !=0:
+        noeud_courant = pile.pop()
+        voisins = recherche_voisins(mat,n_visite,noeud_courant)
+        pile.extend(voisins)
+        n_visite.extend(voisins)
+    return n_visite
+
+print(parcour_profondeur(mat_adj))
+def visiter(u, n_decouverts):
+    adj=[]
+    if u not in n_decouverts:
+        n_decouverts += [u] 
+    for i in recherche_voisins(mat_adj, n_decouverts, u):
+        adj += [i]
+        if i not in n_decouverts:
+            visiter(i, n_decouverts)
+
+def parcour_profondeur_recursif(mat, depart):
+    noeud_decouverts = [depart]
+    visiter(depart, noeud_decouverts)
+    return noeud_decouverts
+
+print(parcour_profondeur_recursif(mat_adj, 0))
+
